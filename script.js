e5c3c9da904829561067aa10e350ed6579bc694e
alejandro <silvaalex921@gmail.com>
2017-12-10 21:10:46 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
+// this is a function for the nav bar when getting to less than 900 px in viewport width //
+// this class gets the id myTopnav and hides all the smaller classes in it and replaces them with the 3 lines //
+// it does this by adding the responsive class in line 9 which hides the html lines and puts the display into block to have them organised differently //
 
 function myFunction() {
     var x = document.getElementById("myTopnav");
